7a9a9cc3f9a88510771802fdef7322aa8618d805
David <topghostly@gmail.com>
2025-05-13 23:52:17 +0100

    feat: Update hero section with video background and styling

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -4,7 +4,7 @@ import { ArrowDownRight } from "lucide-react";
 
 export default function HeroSection() {
   return (
-    <section className="relative overflow-x-hidden">
+    <section className="relative overflow-x-hidden bg-[#05070B]">
       <div className="top-[160px] w-full absolute max-w-[1280px] translate-x-[-50%] left-[50%] pb-[80px] z-10">
         <div className="flex flex-col gap-3">
           <h1 className="font-satoshi-b max-w-[616px]">
@@ -42,53 +42,10 @@ export default function HeroSection() {
           playsInline
           style={{ opacity: 1 }}
         >
-          <source src="/videos/0512.mp4" type="video/mp4" />
+          <source src="/videos/0512(4).mp4" type="video/mp4" />
           {/* <source src="#" type="video/webm" /> */}
         </video>
       </div>
     </section>
-    // <section className="pt-[160px] relative aspect-[1.9] ">
-    //   <div className="container pb-[80px]">
-    //     <div className="flex flex-col gap-3">
-    //       <h1 className="font-satoshi-b max-w-[616px]">
-    //         All You Need to Tell Your Story
-    //       </h1>
-    //       <p className="max-w-[480px]">
-    //         Glyph is a offline-first CMS for seamless storytelling. Draft ideas
-    //         on the go, then sync to the web when you&apos;re ready.
-    //       </p>
-    //       <div className="flex mt-5">
-    //         <Button
-    //           href="#"
-    //           classname="flex gap-[1px] justify-center items-center"
-    //         >
-    //           GET STARTED <ArrowDownRight size={16} strokeWidth={2.5} />
-    //         </Button>
-    //       </div>
-    //     </div>
-    //     <div className="mt-[400px] absolute bottom-0">
-    //       <p className="text-[14px]">
-    //         Everything you need for seamless blogging:
-    //       </p>
-    //     </div>
-    //     {/* <div className="absolute -left-[344px] bottom-0 z-0 aspect-[1.335187] w-[1920px] max-w-none mix-blend-lighten lg:bottom-[23px] lg:left-[-253px] lg:w-[1620px] md:bottom-[-2.1%] md:left-[-27%] md:w-[147%] sm:bottom-[5.4%] sm:left-[-34.95%] sm:w-[189%] xs:bottom-[1.9%] xs:left-[-36.2%] xs:w-[190%] xs:min-w-[704px] 2xs:bottom-[18px] 2xs:left-[-132px] before:absolute before:top-0 before:z-10 before:hidden before:h-20 before:w-full before:bg-gradient-to-b before:from-grey-1 before:to-grey-1/0 sm:before:block bg-amber-700"></div> */}
-    //   </div>
-
-    //   {/* <----------VIDEO BACKGROUND----------> */}
-    //   <div className="absolute w-full h-full max-w-[1920px] left-[50%] translate-x-[-50%] bg-amber-600 top-0 opacity-10">
-    //     <video
-    //       className="absolute inset-0 w-full h-full"
-    //       width="1920"
-    //       height="1438"
-    //       autoPlay
-    //       loop
-    //       playsInline
-    //       style={{ opacity: 1 }}
-    //     >
-    //       <source src="#" type="video/mp4" />
-    //       <source src="#" type="video/webm" />
-    //     </video>
-    //   </div>
-    // </section>
   );
 }
